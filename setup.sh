b2b92110c18934845474ac1f69071479bf06c3d9
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-08-25 21:40:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -755,7 +755,7 @@ TEXT="
 <code>EXP SCRIPT : </code><code>$EXP Days</code>
 <code>━━━━━━━━━━━━━━━━━━━━</code>
 <i> Notifikasi Installer Script...</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/AimanVpnExpress wa.me/+628981874211"},{"text":"GRUP","url":"https://t.me/cloudvpnpremium"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/masansor wa.me/+6281775700114"},{"text":"GRUP","url":"https://t.me/aiyavpnstore"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 clear
 }
